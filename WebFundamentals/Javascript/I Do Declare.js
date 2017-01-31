5423e8dab8eb53bc91eadcbeda3907c2bddf15e6
Zach Gee <Zachsugoi@gmail.com>
2017-01-31 12:18:04 -0800

    added CSS project files

---
+++ WebFundamentals/Javascript/I Do Declare.js
@@ -0,0 +1,18 @@
+var num1 = -36;
+var num2 = 17-4;
+var num3 = 2584;
+var num4 = 6.9;
+var string1 = "Zachariah"
+var string2 = "Helloooo, Nurse"
+var string3 = "4"
+var bool1 = true;
+var bool2 = false;
+var undef = undefined;
+console.log("First Number:",num1);
+console.log("Second Number:",num2);
+console.log("Third Number:",num3);
+console.log("Fourth Number:",num4);
+console.log("First String:",string1);
+console.log("Second String:",string2);
+console.log("Third String:",string3);
+console.log("The undefined one:",undef);
